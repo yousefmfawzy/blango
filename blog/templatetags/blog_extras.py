93c74ecf7ae0dbdc50204da99e92eaab27b19f0b
yousef mamdoh <yousefmaamdoh@gmail.com>
2025-05-04 08:46:03 +0000

    from codio

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+
+register = template.Library()
+user_model = get_user_model()
+
+@register.filter
+def author_details(author):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = "</a>"
+    else:
+        prefix = ""
+        suffix = ""
+
+    return mark_safe(f"{prefix}{name}{suffix}")
